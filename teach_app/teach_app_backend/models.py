017bb8b56269f2c293736411de5d48b0617da236
Rhys <rhys-stephens@hotmail.co.uk>
2020-07-02 18:47:37 +0100

    Created custom user model with manager

--- teach_app/teach_app_backend/models.py
+++ teach_app/teach_app_backend/models.py
@@ -1,3 +1,22 @@
 from django.db import models
+from django.contrib.auth.models import AbstractBaseUser
 
-# Create your models here.
+from teach_app_backend.managers import TeachUserManager
+
+
+class TeachUser(AbstractBaseUser):
+    email = models.EmailField(unique=True)
+    first_name = models.CharField(max_length=32)
+    last_name = models.CharField(max_length=32)
+    profile_picture = models.ImageField(upload_to='profile_pictures')
+
+    is_teacher = models.BooleanField(default=False)
+
+    USERNAME_FIELD = 'email'
+
+    REQUIRED_FIELDS = ['email', 'first_name', 'last_name']
+
+    objects = TeachUserManager()
+
+    def __str__(self):
+        return self.email
