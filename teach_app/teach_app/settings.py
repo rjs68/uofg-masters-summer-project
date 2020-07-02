a82460d7b4e444d0587da518c7eebac871a2f475
Rhys <rhys-stephens@hotmail.co.uk>
2020-07-02 17:48:58 +0100

    Backend django project created

--- teach_app/teach_app/settings.py
+++ teach_app/teach_app/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'teach_app_backend',
 ]
 
 MIDDLEWARE = [
---
+++ teach_app/teach_app_backend/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class TeachAppBackendConfig(AppConfig):
+    name = 'teach_app_backend'
