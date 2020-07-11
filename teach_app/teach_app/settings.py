db51a71ee6e8e1680eef488cd92c3c1d36d4cd72
Rhys <rhys-stephens@hotmail.co.uk>
2020-07-11 14:55:34 +0100

    Created basic connection between react project and django backend

--- teach_app/teach_app/settings.py
+++ teach_app/teach_app/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'teach_app_backend',
+    'teach_app_frontend',
     'rest_framework',
 ]
 
--- teach_app/teach_app_backend/urls.py
+++ teach_app/teach_app_backend/urls.py
@@ -1,9 +1,8 @@
 from django.urls import path
 from teach_app_backend import views
 
-app_name = 'teach'
+app_name = 'teach_app_backend'
 
 urlpatterns = [
-    path('', views.index, name='index'),
     path('api/teach_user', views.TeachUserListCreate.as_view()),
 ]
