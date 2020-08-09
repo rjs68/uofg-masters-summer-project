a53c6180dadbeff1ffe3370a691912720da0d0f0
Rhys <rhys-stephens@hotmail.co.uk>
2020-08-09 12:59:48 +0100

    Accesses webcam for video streaming and displays on local screen

--- teach_app/teach_app_frontend/src/components/LectureVideo/LectureVideo.js
+++ teach_app/teach_app_frontend/src/components/LectureVideo/LectureVideo.js
@@ -3,26 +3,37 @@ import React, { Component } from 'react';
 class LectureVideo extends Component {
     constructor(props){
         super(props);
-        this.state = {
-            constraints : {
-                'video': true,
-                'audio': true
-            }
-        }
+        this.state = {}
+
+        this.getVideoStream = this.getVideoStream.bind(this);
     }
 
-    render() {
-        navigator.mediaDevices.getUserMedia(this.state.constraints)
+    getVideoStream() {
+        const constraints = { audio: true, video: true };
+        navigator.mediaDevices.getUserMedia(constraints)
             .then(stream => {
                 console.log('Got MediaStream:', stream);
+                var video = document.querySelector('video');
+                video.srcObject = stream;
+                video.onloadedmetadata = () => {
+                    video.play();
+                };
             })
             .catch(error => {
                 console.error('Error accessing media devices.', error);
             });
-            
+    }
+
+    componentDidMount(){
+        this.getVideoStream();
+    }
+
+    render() {
         return (
-            <div>Video</div>
-        );
+            <div>
+                <video autoPlay={true} id='lectureVideo' controls></video>
+            </div>
+        )
     }
 }
 
